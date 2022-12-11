64ace958dbe9be1f30400795ff899e8cd488945b
Garvin O'Brien <110465736+garvbox@users.noreply.github.com>
2022-12-11 14:48:29 +0000

    Install Pre-commit automatically

--- install.sh
+++ install.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
 zshrc() {
-    echo "==========================================================="
-    echo "             cloning zsh-autosuggestions                   "
-    echo "-----------------------------------------------------------"
+    echo "cloning zsh-autosuggestions."
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    echo "==========================================================="
-    echo "             cloning zsh-syntax-highlighting               "
-    echo "-----------------------------------------------------------"
+    echo "cloning zsh-syntax-highlighting."
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    echo "==========================================================="
-    echo "             cloning powerlevel10k                         "
-    echo "-----------------------------------------------------------"
+    echo "cloning powerlevel10k."
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    echo "==========================================================="
-    echo "             import zshrc                                  "
-    echo "-----------------------------------------------------------"
+    echo "import zshrc."
     cat .zshrc > $HOME/.zshrc
 }
 
 zshrc
+
+# Pre-Commit Install and configure
+echo "Installing Pre-Commit."
+pip install pre-commit
