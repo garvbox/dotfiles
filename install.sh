690f452abff477ee4c58ff9ec365e93ca0a8e581
Garvin O'Brien <110465736+garvbox@users.noreply.github.com>
2022-12-21 14:10:43 +0000

    Fix install for codespaces
    Linking wont work here as we dont have the source paths,
    just cat to output

--- install.sh
+++ install.sh
@@ -20,15 +20,15 @@ get_zsh_plugin ${zsh_custom}/themes/powerlevel10k "--depth=1 https://github.com/
 if [[ -f $HOME/.zshrc ]]; then
     echo "zshrc already exists, not updating"
 else
-    echo "Linking zshrc."
-    ln -s .zshrc $HOME/.zshrc
+    echo "Creating zshrc."
+    cat .zshrc > $HOME/.zshrc
 fi
 
 if [[ -f $HOME/.gitconfig ]]; then
     echo "gitconfig already exists, not updating"
 else
-    echo "Linking .gitconfig"
-    ln -s .gitconfig $HOME/.gitconfig
+    echo "Updating .gitconfig"
+    cat .gitconfig > $HOME/.gitconfig
 fi
 
 # Pre-Commit Install and configure
