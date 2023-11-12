0deb1318ffe20864e8740652657a2f1c9909467c
garvinob <garv@garvbox.net>
2023-11-12 12:27:05 +0000

    Modularise nvim config
    Inspiration taken from kickstart-modular nvim repo, some
    small tweaks. Manually copied all content from AIO kickstart
    version to practise nav.

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,30 @@
+-- Helper to shorten function name
+local keymap = vim.keymap.set
+
+keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Reset cursor to center when scrolling
+keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+
+
+-- Remap for dealing with word wrap
+keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
