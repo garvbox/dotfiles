e2c7210844e22c56f27b32126a11ba2397d97e18
garvinob <garv@garvbox.net>
2024-03-03 21:37:25 +0000

    Remove neodev

--- nvim/lua/lazy-setup.lua
+++ nvim/lua/lazy-setup.lua
@@ -54,13 +54,11 @@ require('lazy').setup({
       -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
     },
   },
 
