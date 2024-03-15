ccf98613d9524531cf6b018e4d37c383052180f6
garvbox <garv@garvbox.net>
2024-03-15 17:24:58 +0000

    Download all Catppuccin themes

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ main() {
 	download_alacritty_themes
 	install_tmux_tpm
 	bat_setup
+
+	# Codespaces are a fairly specific environment, we can pretty much guarantee that they are 
+	if [[ -n $CODESPACES ]]; then
+		echo "Running Codespace-Specific Setup"
+	fi
 }
 
 command_exists() {
@@ -34,24 +39,31 @@ install_tmux_tpm() {
 download_alacritty_themes() {
 	if [[ ! -d $HOME/.config/alacritty ]]; then
 		mkdir $HOME/.config/alacritty
-		wget -P $HOME/.config/alacritty \
-			https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
-	else
-		echo "Alacritty Config already exists, skipped downloading theme"
 	fi
+	for theme in latte frappe macchiato mocha; do
+		theme_file="catppuccin-${theme}.toml"
+		if [[ ! -f $HOME/.config/alacritty/${theme_file} ]]; then
+			wget -P $HOME/.config/alacritty \
+				https://github.com/catppuccin/alacritty/raw/main/${theme_file}
+		fi
+	done
 }
 
 bat_setup () {
 	if [[ ! -d $HOME/.config/bat/themes ]]; then
-		mkdir $HOME/.config/bat
-		wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+		mkdir -p $HOME/.config/bat
+		for theme in Mocha Latte Frappe Macchiato; do
+			cp_theme_file="Catppuccin ${theme}.tmTheme"
+			cp_theme_dl="Catppuccin%20${theme}.tmTheme"
+			if [[ ! -f ${bat_theme_path}/${cp_theme_file} ]]; then
+				wget -P "$HOME/.config/bat/themes" https://github.com/catppuccin/bat/raw/main/themes/${cp_theme_dl}
+			fi
+		done
 		if command_exists bat; then
 			bat cache --build
 		else
 			echo "Bat is not installed, cache not updated"
 		fi
-	else
-		echo "Bat Themes already exist, skipped setup"
 	fi
 }
 
