cbe0a671352c9a1bf548b63876418ecb027c1021
Garvin <garv@garvbox.net>
2024-03-16 17:31:31 +0000

    Add fish abbreviations

---
+++ common/.config/fish/conf.d/abbrevs.fish
@@ -0,0 +1,19 @@
+
+# Git Abbrevs
+abbr -a gco "git checkout"
+abbr -a gcb "git checkout -b"
+abbr -a gfa "git fetch --all --prune"
+abbr -a gca "git commit --verbose --all"
+abbr -a gd "git diff"
+abbr -a gl "git pull"
+abbr -a gp "git push"
+abbr -a gst "git status"
+
+# Editor
+alias vim="nvim"
+
+# General Shell Usage
+# Rusty tools :-)
+alias ls="exa"
+alias cat="bat"
+
