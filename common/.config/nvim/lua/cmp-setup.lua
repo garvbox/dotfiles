435c07b5d1e21d81f7f34a0e3971c7a3374959aa
Garvin <garv@garvbox.net>
2024-03-16 15:40:27 +0000

    Re-enable CMP snippets
    Snippet engine is needed for any sort of selection
    to work :(

--- common/.config/nvim/lua/cmp-setup.lua
+++ common/.config/nvim/lua/cmp-setup.lua
@@ -1,8 +1,16 @@
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
 
 cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
   mapping = cmp.mapping.preset.insert {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -15,6 +23,7 @@ cmp.setup {
   },
   sources = {
     { name = 'nvim_lsp' },
+    { name = 'luasnip' },
     { name = 'path' },
   },
 }
--- common/.config/nvim/lua/lazy-plugins/cmp.lua
+++ common/.config/nvim/lua/lazy-plugins/cmp.lua
@@ -6,5 +6,7 @@ return {
     -- Adds LSP completion capabilities
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip'
   },
 }
