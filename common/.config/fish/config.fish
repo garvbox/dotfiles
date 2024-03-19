c878dac94096f74c78edbad2abfaf23e5471819e
garvinobrien <garvin.obrien@keelvar.com>
2024-03-19 19:19:38 +0000

    Add paths

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -3,6 +3,8 @@ set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path $HOME/.cargo/bin
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/bin
 end
 
 
