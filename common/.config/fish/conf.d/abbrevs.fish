06d41e7120156e2b290bfc835138cff78bf9813f
garvinobrien <garvin.obrien@keelvar.com>
2024-04-10 15:08:55 +0100

    Remove poetry abbrev
    This caused some issues with the LSP, when it restarts
    it would be missing environment info so now we always
    do poetry shell before starting vim

--- common/.config/fish/conf.d/abbrevs.fish
+++ common/.config/fish/conf.d/abbrevs.fish
@@ -14,7 +14,6 @@ abbr -a gfp "git commit --amend --no-edit && git push --force"
 # Misc
 alias vim="nvim"
 abbr -a k "kubectl"
-abbr -a pyvim "poetry run nvim"
 # Granted
 alias assume="source (brew --prefix)/bin/assume.fish"
 
