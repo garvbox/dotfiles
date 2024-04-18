409e1339f92aa59bdce3440bfefc7cbf011347af
garvinobrien <garvin.obrien@keelvar.com>
2024-04-18 18:07:19 +0100

    Remove CR for cmp suggestions
    Consistent newline beahviour, force to use C-Y for accept

--- common/.config/nvim/lua/cmp-setup.lua
+++ common/.config/nvim/lua/cmp-setup.lua
@@ -16,10 +16,6 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-y>'] = cmp.mapping.confirm { select = true },
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
   },
   sources = {
     { name = 'nvim_lsp' },
