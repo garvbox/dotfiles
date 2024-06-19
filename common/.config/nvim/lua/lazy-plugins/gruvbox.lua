59fcad4cfda9ca78e6da9f8ecffc25c96a0a4ec3
garvinobrien <garvin.obrien@keelvar.com>
2024-06-19 15:19:24 +0100

    Revert "Switch to catppuccin-latte"
    This reverts commit 3cb908deb8a579986d5cf25d15138104c715c8b7.
    
    catppucin was too hard to read in nvim, taking the hit on terminal
    visibility for better display in editor

--- common/.config/nvim/lua/lazy-plugins/gruvbox.lua
+++ common/.config/nvim/lua/lazy-plugins/gruvbox.lua
@@ -2,7 +2,7 @@ return {
   'ellisonleao/gruvbox.nvim',
   priority = 1000,
   opts = {},
-  -- config = function()
-  --   vim.cmd.colorscheme 'gruvbox'
-  -- end,
+  config = function()
+    vim.cmd.colorscheme 'gruvbox'
+  end,
 }
