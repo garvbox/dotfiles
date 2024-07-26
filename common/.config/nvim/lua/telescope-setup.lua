7fb3685050bbd607c59ccf9b36f06b7eaec9b9d8
garvinobrien <garvin.obrien@keelvar.com>
2024-07-26 10:39:12 +0100

    Add keymap for telescope buffers

--- common/.config/nvim/lua/telescope-setup.lua
+++ common/.config/nvim/lua/telescope-setup.lua
@@ -41,6 +41,7 @@ vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]re
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
 
 vim.keymap.set('n', '<leader>/', function()
   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
