b6e801acb128b9ac106f768e07ff6ab3d65dd1af
garvinobrien <garvin.obrien@keelvar.com>
2024-08-27 11:02:12 +0100

    Add Wezterm initial config
    Use gruvbox and same fonts to start with

---
+++ common/.wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'GruvboxDark'
+config.font = wezterm.font 'Hack Nerd Font'
+config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
