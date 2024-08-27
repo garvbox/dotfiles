7efad5e3417654e13ea52dade4e65c31d13a6be8
garvinobrien <garvin.obrien@keelvar.com>
2024-08-27 19:30:08 +0100

    Fix font size

--- common/.wezterm.lua
+++ common/.wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'GruvboxDark'
 config.font = wezterm.font 'Hack Nerd Font'
-config.font_size = 14
+config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
