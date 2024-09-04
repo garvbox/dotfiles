0443e6d9a083845bb7a26e2e32a2d97674a79ac4
garvinobrien <garvin.obrien@keelvar.com>
2024-09-04 14:59:24 +0100

    Add neotest testing plugin
    This is used in lazyvim and might be a nice testing helper for python
    projects

---
+++ common/.config/nvim/lua/lazy-plugins/neotest.lua
@@ -0,0 +1,44 @@
+local Path = require("plenary.path")
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-python"
+  },
+  config = function()
+    local keymap = vim.keymap.set
+    local nt = require("neotest")
+
+    nt.setup({
+      adapters = {
+        require("neotest-python")({
+          is_test_file = function(file_path)
+            if not vim.endswith(file_path, ".py") then
+              return false
+            end
+            local elems = vim.split(file_path, Path.path.sep)
+            local file_name = elems[#elems]
+            return vim.startswith(file_name, "test_") or vim.endswith(file_name, "_test.py") or
+                vim.endswith(file_name, "_tests.py")
+          end
+        })
+      },
+    })
+
+    -- Setup Keymaps
+    keymap('n', "<leader>t", "", { desc = "+test" })
+    keymap('n', "<leader>tt", function() nt.run.run(vim.fn.expand("%")) end, { desc = "Run File" })
+    keymap('n', "<leader>tT", function() nt.run.run(vim.uv.cwd()) end, { desc = "Run All Test Files" })
+    keymap('n', "<leader>tr", function() nt.run.run() end, { desc = "Run Nearest" })
+    keymap('n', "<leader>tl", function() nt.run.run_last() end, { desc = "Run Last" })
+    keymap('n', "<leader>ts", function() nt.summary.toggle() end, { desc = "Toggle Summary" })
+    -- keymap('n', "<leader>to", function() nt.output.open(keymap('n', enter = true, auto_close = true }) end, {desc = "Show Output"} )
+    keymap('n', "<leader>tO", function() nt.output_panel.toggle() end, { desc = "Toggle Output Panel" })
+    keymap('n', "<leader>tS", function() nt.run.stop() end, { desc = "Stop" })
+    keymap('n', "<leader>tw", function() nt.watch.toggle(vim.fn.expand("%")) end,
+      { desc = "Toggle Watch" })
+  end,
+}
