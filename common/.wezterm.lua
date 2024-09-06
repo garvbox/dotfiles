b5ccae580fccffa70f467892df4a7141470fd60e
garvinobrien <garvin.obrien@keelvar.com>
2024-09-06 16:31:23 +0100

    Set renderer frontend to match alacritty rendering

--- common/.wezterm.lua
+++ common/.wezterm.lua
@@ -6,5 +6,8 @@ config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+-- This is supposed to be the default setting, but somehow it needs to be set,
+-- otherwise the fonts look skinny and weird next to alacritty
+config.front_end = "WebGpu"
 
 return config
