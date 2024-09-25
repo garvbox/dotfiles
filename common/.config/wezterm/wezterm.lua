8bfc59b96bda15adf4df2cf2d87fe1591d66e0c3
garvinobrien <garvin.obrien@keelvar.com>
2024-09-25 19:09:34 +0100

    Set initial size for wezterm

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -6,6 +6,8 @@ config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+config.initial_rows = 48
+config.initial_cols = 160
 -- This is supposed to be the default setting, but somehow it needs to be set,
 -- otherwise the fonts look skinny and weird next to alacritty
 config.front_end = "WebGpu"
