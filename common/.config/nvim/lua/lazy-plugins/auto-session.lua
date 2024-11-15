dcd3797f8da94cd2ec9d165c2ade93af41cc5f3e
garvinobrien <garvin.obrien@keelvar.com>
2024-11-15 10:06:04 +0000

    Enable auto-session auto-restore
    I no longer have a fancy greeter screen so may as well auto-restore

--- common/.config/nvim/lua/lazy-plugins/auto-session.lua
+++ common/.config/nvim/lua/lazy-plugins/auto-session.lua
@@ -2,10 +2,9 @@ return {
   'rmagatti/auto-session',
   lazy = false,
   dependencies = {
-    'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
+    'nvim-telescope/telescope.nvim',
   },
   opts = {
     auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    auto_restore_enabled = false
   }
 }
