a99f2f88ec38e7d1969537361448cfc08233563e
garvinobrien <garvin.obrien@keelvar.com>
2024-11-15 11:44:34 +0000

    Switch to Catppuccin theme for all

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'GruvboxDark'
+config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
