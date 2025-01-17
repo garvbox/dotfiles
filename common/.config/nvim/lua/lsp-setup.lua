e4a8f56bf7aac6773c2637ff3a098a78e10d5db7
garvinobrien <garvin.obrien@keelvar.com>
2025-01-17 17:33:30 +0000

    Add markdown renderer

--- common/.config/nvim/lua/lsp-setup.lua
+++ common/.config/nvim/lua/lsp-setup.lua
@@ -57,6 +57,7 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
+  marksman = {},
   pyright = {
     pyright = {
       disableOrganizeImports = true,
