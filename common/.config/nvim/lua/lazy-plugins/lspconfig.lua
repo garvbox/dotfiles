14361137432590936087e17684f80edac8ae5d65
garvinobrien <garvin.obrien@keelvar.com>
2025-05-08 16:36:43 +0100

    Pin mason to v1

--- common/.config/nvim/lua/lazy-plugins/lspconfig.lua
+++ common/.config/nvim/lua/lazy-plugins/lspconfig.lua
@@ -4,8 +4,8 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
     -- Automatically install LSPs to stdpath for neovim
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+    { "mason-org/mason.nvim",           version = "^1.0.0" },
+    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
     'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     -- Useful status updates for LSP
