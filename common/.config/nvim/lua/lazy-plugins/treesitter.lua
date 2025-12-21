2f0a490364d3c47d984f853f7cb2db432a37f4e2
garvbox <garv@garvbox.net>
2025-12-21 16:49:05 +0000

    Pin treesitter to master branch
    The default has switched to main which has a lot of breaking changes

--- common/.config/nvim/lua/lazy-plugins/treesitter.lua
+++ common/.config/nvim/lua/lazy-plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
