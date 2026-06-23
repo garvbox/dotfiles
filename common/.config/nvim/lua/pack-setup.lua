ad7410a666b086c50d83b94daec96b48c37e7118
garvinobrien <garvin.obrien@keelvar.com>
2026-06-23 13:08:28 +0100

    Add Obsidian.nvim
    Uses auto-workspace setup, to be overridden locally

--- common/.config/nvim/lua/pack-setup.lua
+++ common/.config/nvim/lua/pack-setup.lua
@@ -36,6 +36,8 @@ vim.pack.add {
   'https://github.com/folke/todo-comments.nvim',
   'https://github.com/folke/trouble.nvim',
 
+  'https://github.com/obsidian-nvim/obsidian.nvim',
+
   'https://github.com/nvim-telescope/telescope.nvim',
   'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
   'https://github.com/nvim-telescope/telescope-ui-select.nvim',
@@ -184,6 +186,19 @@ vim.api.nvim_create_autocmd('UIEnter', {
     vim.keymap.set('n', '<leader>S', function()
       Snacks.scratch.select()
     end, { desc = 'Select Scratch Buffer' })
+
+    require('obsidian').setup {
+      picker = { name = 'telescope.nvim' },
+      legacy_commands = false,
+      workspaces = {
+        {
+          name = 'buf-parent',
+          path = function()
+            return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+          end,
+        },
+      },
+    }
 
     vim.g.startuptime_tries = 10
   end,
