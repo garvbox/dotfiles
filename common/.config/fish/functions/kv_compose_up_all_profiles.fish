d5f218ba01c33ebc0b3f783cf6796ef5116ff924
garvinobrien <garvin.obrien@keelvar.com>
2026-09-21 09:36:16 +0100

    Update compose helper to disable build if desired

--- common/.config/fish/functions/kv_compose_up_all_profiles.fish
+++ common/.config/fish/functions/kv_compose_up_all_profiles.fish
@@ -1,8 +1,16 @@
-function kv_compose_up_all_profiles
+function kv_compose_up_all_profiles -d 'Bring up every COMPOSE_PROFILES profile detached, rebuilding images unless --no-build is given'
+  argparse no-build -- $argv
+  or return
+
+  set -l up_args -d
+  if not set -q _flag_no_build
+    set -a up_args --build
+  end
+
   echo "Bringing services up: $COMPOSE_PROFILES"
 
   for profile in $(string split ',' $COMPOSE_PROFILES)
     echo "################# $profile ######################"
-    docker compose --profile $profile up -d --build
+    docker compose --profile $profile up $up_args
   end
 end
